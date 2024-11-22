be947d4dda80bb418b766071a4a4459127c6fea8
VitorLostadaC <vitorlostada@hotmail.com>
2024-11-21 21:39:33 -0300

    fix: build

--- src/app/projects/component/ProjectCard.tsx
+++ src/app/projects/component/ProjectCard.tsx
@@ -1,4 +1,4 @@
-import { Card, CardContent, CardHeader, CardTitle } from '@/components/ui/card'
+import { Card, CardContent, CardHeader } from '@/components/ui/card'
 import { Project } from '../data'
 import { ProjectStack } from './components/ProjectStack'
 
