7aad92b0e0c7df6b708f28fb21ed98afc6e3a538
VitorLostadaC <vitorlostada@hotmail.com>
2024-12-07 08:18:44 -0300

    feat: add consai texts

--- src/app/projects/(details)/consai/data/project.tsx
+++ src/app/projects/(details)/consai/data/project.tsx
@@ -4,7 +4,18 @@ import { RoughNotation as RN } from 'react-rough-notation'
 export const heroTitle = 'Consai'
 
 export const heroDescription = (
-  <>Consai is my first SASS application with entuit to earn money,</>
+  <>
+    Consai is my first SaaS application, made to try and earn money, and this
+    changed all our decisions. I built it with my friend{' '}
+    <a className="underline" href="https://www.linkedin.com/in/leomconti/">
+      Leo
+    </a>
+    . We found an opportunity with AI and decided to create this SaaS to try
+    selling it to other businesses. We needed to ship fast to get our space in
+    the market. I built the whole application (backend and frontend), and he
+    worked on the AI API. We had many challenges and had to stop thinking like
+    developers and start thinking like CEOs.
+  </>
 )
 
 export const tags: Tag[] = [
@@ -24,39 +35,9 @@ export const goalTitle = 'Project Purpose and Goal'
 
 export const goalDescription = [
   <>
-    I created this project with two main objectives. First, to build a{' '}
-    <RN type="underline" color="var(--highlight-1)" multiline>
-      complete SaaS application
-    </RN>{' '}
-    from scratch - going through{' '}
-    <RN type="underline" multiline color="var(--highlight-2)">
-      every stage from development to production deployment.
-    </RN>{' '}
-    Second, I wanted to learn more about the new Next.js 14 , including server
-    actions and how to do self-hosting with Next.js . I had also never used
-    Stripe and S3 before, so I had to learn a lot of new things and implement
-    them in this project.
-  </>,
-
-  <>
-    Event is a SaaS project, but{' '}
-    <RN type="underline" multiline color="var(--highlight-1)">
-      the main purpose was not monetization.
-    </RN>{' '}
-    I don&apos;t even have any ads or affiliate links in this project. It was
-    simply an{' '}
-    <RN type="underline" multiline color="var(--highlight-2)">
-      enjoyable project to build
-    </RN>{' '}
-    and a great opportunity to learn many new things.
-  </>,
-  <>
-    I tried to create a{' '}
-    <RN type="box" multiline color="var(--highlight-2)">
-      minimalist design
-    </RN>{' '}
-    with a focus on UX. Since most users struggle to write good prompts, I
-    created a guided flow to help them achieve better results.
+    Our main goal was try get max money as possible, and build a strong
+    alternative for the solutions already existents, we wanted understand more
+    about market, seels, marketing and how grow a product
   </>
 ]
 
@@ -64,120 +45,67 @@ export const problemsTitle = 'Problems and Thought Process'
 
 export const problemsDescription = [
   <>
-    I don’t even know where to start because I really faced several challenges
-    when starting this project. The first issue I encountered was{' '}
-    <RN type="underline" multiline color="var(--highlight-1)">
-      authentication in Next.js
-    </RN>{' '}
-    because it involves two environments: server and client. To solve this, I
-    searched all over the internet, focusing on{' '}
-    <RN type="box" multiline color="var(--highlight-2)">
-      GitHub repositories from people who work with Next.js
-    </RN>
-    .
+    We built the project like it was for a big company. At the start, I used the
+    best practices and design patterns, but the project took too long to build
+    and ship. Because of that, we couldn&apos;t release or test it to see if the
+    product was useful. The first thing I changed was moving from SOLID
+    principles and use cases to a simple MVC, and this made everything faster to
+    build.
   </>,
-
   <>
-    Self-hosting: I am using a{' '}
-    <RN type="circle" multiline color="var(--highlight-1)">
-      VPN
-    </RN>{' '}
-    to host everything for my project, and some features of Vercel are not
-    available. To address this, I had to find a solution for good loading
-    speeds. I implemented a{' '}
-    <RN type="circle" multiline color="var(--highlight-2)">
-      CDN
-    </RN>{' '}
-    to host some cached pages, which keeps my landing page very fast.
+    Since we were working in our free time, we hired an intern to speed things
+    up. I planned their tasks so they could work on things that weren&apos;t
+    worth *our* time, like improving the UI and making it responsive. Meanwhile,
+    I focused on the core logic and main features of the product.
   </>,
-
   <>
-    Performance : At first, my landing page looked fine, but when I ran
-    Lighthouse,{' '}
-    <RN type="underline" multiline color="var(--highlight-1)">
-      the results were very poor
-    </RN>{' '}
-    haha. I started an extensive search to understand how to improve it. I{' '}
-    <RN type="underline" multiline color="var(--highlight-2)">
-      converted all images to WebP to optimize their size
-    </RN>{' '}
-    and resized them to match the actual rendered dimensions. This improved the
-    performance scores significantly.
-  </>,
-
-  <>
-    This bug was only discovered in{' '}
-    <RN type="circle" multiline color="var(--highlight-1)">
-      production
-    </RN>
-    . The generated images were{' '}
-    <RN type="underline" multiline color="var(--highlight-2)">
-      extremely slow to render
-    </RN>{' '}
-    on users&apos; screens because I wasn&apos;t optimizing the images. To
-    quickly fix this, I used{' '}
-    <RN type="underline" color="var(--highlight-1)">
-      Sharp
-    </RN>{' '}
-    to resize the images on the frontend. The solution worked, but{' '}
-    <RN type="underline" color="var(--highlight-1)">
-      it&apos;s not ideal
-    </RN>{' '}
-    because{' '}
-    <RN type="underline" multiline color="var(--highlight-2)">
-      it added extra load time on the frontend
-    </RN>
-    . For example, if you go to the gallery, you&apos;ll notice a long loading
-    time because I&apos;m resizing all images when the user opens the page. I
-    could fix this in several ways, such as{' '}
-    <RN type="underline" multiline color="var(--highlight-1)">
-      generating resized WebP images on the backend
-    </RN>{' '}
-    or creating a job to run after generation and save on db to show the user
-    the result immediately. Unfortunately, I haven&apos;t had time to fix this
-    yet, and since we currently don&apos;t have active users, this refactor
-    won&apos;t have much of an impact for now.
+    Another challenge was selling the product. We had to figure out how to do
+    it. We started talking to people who could join us as affiliates and sell
+    the product for us. This was an interesting step because it helped us
+    connect with many people in the market.
   </>
+]
+
+export const keyFeaturesTitle = 'Key Features'
+
+export const keyFeaturesDescription = [
+  <>
+    Created a strong SaaS template with authentication, Stripe integration for
+    plan limits, organization and project management, team invites, a landing
+    page/app structure, and a fast script to configure a new project structure
+    in literally 2 seconds.
+  </>,
+  <>
+    An embed that can be added to your website, fully customized to match your
+    brand.
+  </>,
+  <>
+    A real-time chat where clients can pause the AI and take over the
+    conversation themselves.
+  </>,
+  <>A playground page to test the agents.</>,
+  <>Beautiful animations to make the experience smoother 🤩.</>
 ]
 
 export const lessonsTitle = 'Lessons Learned'
 
 export const lessonsDescription = [
   <>
-    This project was really awesome because I learned a lot about{' '}
-    <RN type="underline" multiline color="var(--highlight-1)">
-      all the technologies I wanted to use
-    </RN>
-    , and also learned about{' '}
-    <RN type="underline" multiline color="var(--highlight-2)">
-      product development and how to ship a project
-    </RN>
-    .
-  </>,
-
-  <>
-    While I didn&apos;t aim to create a lucrative business or run ads, just
-    having users from{' '}
-    <RN type="underline" multiline color="var(--highlight-1)">
-      LinkedIn and Instagram
-    </RN>{' '}
-    gave me great experience working with a production product. For example,
-    when I first shipped the app, I discovered it wasn&apos;t opening properly
-    in Safari{' '}
-    <RN type="underline" multiline color="var(--highlight-2)">
-      and had to quickly fix bugs in production
-    </RN>{' '}
+    I learned a lot about business because the final user doesn&apos;t even know
+    which technology we are using. One big takeaway for me is understanding the
+    timing of a product. If it&apos;s an MVP to test an idea, some things are
+    more important than creating perfect, maintainable code.
   </>,
   <>
-    Being a side project, my time to work on this and create the best possible
-    experience was limited. However, I&apos;m{' '}
-    <RN type="box" multiline color="var(--highlight-1)">
-      really happy with the results
-    </RN>{' '}
-    and everything I learned. This experience{' '}
-    <RN type="underline" multiline color="var(--highlight-2)">
-      contributed significantly to my growth as a developer
-    </RN>
-    .
+    If I build another app, I will focus on getting clients before making the
+    product. That way, I can create something the market is actually interested
+    in. For example, I would build a landing page with a &apos;join on
+    waitlist&apos; button to test market interest.
+  </>,
+  <>
+    When you teach people, their performance doesn&apos;t just double it grows
+    even more. Over time, the people you hire start to understand how to get
+    things done, and they become self-driven to complete tasks and even create
+    their own tasks.
   </>
 ]
