e9c30c86ba167cae54eb3c4395d8d57835284f0b
VitorLostadaC <vitorlostada@hotmail.com>
2024-12-29 11:48:52 -0300

    fix: eslint

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,18 +1,22 @@
-import path from "node:path";
-import { fileURLToPath } from "node:url";
-import js from "@eslint/js";
-import { FlatCompat } from "@eslint/eslintrc";
+import path from 'node:path'
+import { fileURLToPath } from 'node:url'
+import js from '@eslint/js'
+import { FlatCompat } from '@eslint/eslintrc'
 
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
+const __filename = fileURLToPath(import.meta.url)
+const __dirname = path.dirname(__filename)
 const compat = new FlatCompat({
-    baseDirectory: __dirname,
-    recommendedConfig: js.configs.recommended,
-    allConfig: js.configs.all
-});
+  baseDirectory: __dirname,
+  recommendedConfig: js.configs.recommended,
+  allConfig: js.configs.all
+})
 
-export default [...compat.extends("next/core-web-vitals", "next/typescript"), {
+export default [
+  ...compat.extends('next/core-web-vitals', 'next/typescript'),
+  {
     rules: {
-        "@next/next/no-img-element": "off",
-    },
-}];
+      '@next/next/no-img-element': 'off',
+      'react/jsx-key': 'off'
+    }
+  }
+]
