f69a5a1a7ae7f6996f7c3a4d0013e970936efcb7
VitorLostadaC <vitorlostada@hotmail.com>
2024-12-29 10:54:29 -0300

    test: debug metadata

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,4 +1,4 @@
-// import type { Metadata } from 'next'
+import type { Metadata } from 'next'
 import { SpeedInsights } from '@vercel/speed-insights/next'
 import { Analytics } from '@vercel/analytics/react'
 import { Inter } from 'next/font/google'
@@ -11,36 +11,36 @@ const geistMono = Inter({
   subsets: ['latin']
 })
 
-// const siteTitle = 'Vitor Lostada'
-// const siteDescription =
-//   'Frontend Developer crafting pixel-perfect and type-safe web experiences with React and Next.js.'
-// const siteImage = {
-//   url: '/SEO/hero.png',
-//   width: 1200,
-//   height: 630,
-//   alt: 'Vitor Lostada - Frontend Developer'
-// }
+const siteTitle = 'Vitor Lostada'
+const siteDescription =
+  'Frontend Developer crafting pixel-perfect and type-safe web experiences with React and Next.js.'
+const siteImage = {
+  url: '/SEO/hero.png',
+  width: 1200,
+  height: 630,
+  alt: 'Vitor Lostada - Frontend Developer'
+}
 
-// export const metadata: Metadata = {
-//   title: siteTitle,
-//   description: siteDescription,
-//   openGraph: {
-//     images: [siteImage]
-//   },
-//   metadataBase: new URL('https://vitorlostada.com'),
-//   twitter: {
-//     card: 'summary_large_image',
-//     site: 'https://vitorlostada.com',
-//     creator: 'https://vitorlostada.com',
-//     title: siteTitle,
-//     description: siteDescription,
-//     images: [siteImage]
-//   },
-//   robots: {
-//     index: true,
-//     follow: true
-//   }
-// }
+export const metadata: Metadata = {
+  title: siteTitle,
+  description: siteDescription,
+  openGraph: {
+    images: [siteImage]
+  },
+  metadataBase: new URL('https://vitorlostada.com'),
+  twitter: {
+    card: 'summary_large_image',
+    site: 'https://vitorlostada.com',
+    creator: 'https://vitorlostada.com',
+    title: siteTitle,
+    description: siteDescription,
+    images: [siteImage]
+  },
+  robots: {
+    index: true,
+    follow: true
+  }
+}
 
 export default function RootLayout({
   children
