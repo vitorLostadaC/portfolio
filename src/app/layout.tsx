b1bc65164be26214b4505ce498d185ef060b0aa7
VitorLostadaC <vitorlostada@hotmail.com>
2024-12-29 09:14:04 -0300

    refactor: seo

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -15,7 +15,7 @@ const siteTitle = 'Vitor Lostada'
 const siteDescription =
   'Frontend Developer crafting pixel-perfect and type-safe web experiences with React and Next.js.'
 const siteImage = {
-  url: '/SEO/hero.png',
+  url: 'https://vitorlostada.com/SEO/hero.png',
   width: 1200,
   height: 630,
   alt: 'Vitor Lostada - Frontend Developer'
