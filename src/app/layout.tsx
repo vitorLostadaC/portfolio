4b58e5821e1682939dfc314b5cbd4460ba7879bd
VitorLostadaC <vitorlostada@hotmail.com>
2024-12-29 10:41:46 -0300

    test: debug metadata

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,4 +1,4 @@
-import type { Metadata } from 'next'
+// import type { Metadata } from 'next'
 import { SpeedInsights } from '@vercel/speed-insights/next'
 import { Analytics } from '@vercel/analytics/react'
 import { Inter } from 'next/font/google'
