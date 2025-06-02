e52a5af85a2c1e96ded9cddbc4bbfdb4cb0c982b
VitorLostadaC <vitorlostada@hotmail.com>
2025-06-02 09:56:32 -0300

    feat: add deprecated

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,11 +1,13 @@
-import type { Metadata } from 'next'
-import { SpeedInsights } from '@vercel/speed-insights/next'
-import { Analytics } from '@vercel/analytics/react'
-import { Inter } from 'next/font/google'
-import './globals.css'
-import { Header } from '@/layout/Header'
 import { Feedback } from '@/components/feedback/feedback'
 import { PostHogProviderWrapper } from '@/contexts/PosthogWrapper'
+import { Header } from '@/layout/Header'
+import { Analytics } from '@vercel/analytics/react'
+import { SpeedInsights } from '@vercel/speed-insights/next'
+import type { Metadata } from 'next'
+import { Inter } from 'next/font/google'
+import { Toaster } from 'sonner'
+import { Deprecated } from './deprecated'
+import './globals.css'
 
 const geistMono = Inter({
   subsets: ['latin']
@@ -56,9 +58,11 @@ export default function RootLayout({
           <SpeedInsights />
           <Analytics />
           <Feedback />
+          <Toaster />
 
           <div className="relative flex min-h-screen flex-col">
             <Header />
+            <Deprecated />
             <main className="flex-1 px-4 py-10 lg:py-16">{children}</main>
           </div>
         </body>
