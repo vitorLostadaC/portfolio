84d99fdea5e4901d53219795b1d2b5c395a1b1c4
VitorLostadaC <vitorlostada@hotmail.com>
2026-01-05 21:26:58 -0300

    fix: remove toast

--- src/app/deprecated.tsx
+++ src/app/deprecated.tsx
@@ -1,17 +1,6 @@
 'use client'
 
-import { useEffect } from 'react'
-import { toast } from 'sonner'
-
 export const Deprecated = () => {
-  useEffect(() => {
-    setTimeout(() => {
-      toast.error("This portfolio is deprecated. I'm working on new one.", {
-        position: 'top-right',
-        duration: 10000
-      })
-    }, 1000)
-  }, [])
   return (
     <div>
       <h1>Deprecated</h1>
